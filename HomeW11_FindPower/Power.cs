efb8c18b84b512b3574d53f294fc5c6fbf9e344a
msaadat03 <saadatam@code.edu.az>
2022-07-03 22:15:48 +0300

    HomeW11

---
+++ HomeW11_FindPower/Power.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeW11_FindPower
+{
+    public static class Power
+    {
+        public static double Powering(this double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+
+    }
+} 
+ 
---
+++ HomeW11_FindPower/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeW11_FindPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 2;
+            double num2 = 5;
+            {
+                Console.WriteLine(num1.Powering(num2));
+            }
+        }
+    }
+}
+
